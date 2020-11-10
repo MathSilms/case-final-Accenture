4c392e502723578169b30ba7fe5091e6fe73463a
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2020-11-10 02:18:01 -0300

    criando base da aplicação

---
+++ src/pages/cadastro/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Cadastro:React.FC = () =>{
+
+return <h1>Cadastro</h1>;
+
+};
+
+export default Cadastro;
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login:React.FC = () =>{
+
+return <h1>Login</h1>;
+
+};
+
+export default Login;
