442cf7f1ccddef08f60f0aacd9b8754a4581e5d4
Matheus Rian <souzamatheusrian@gmail.com>
2020-11-13 14:04:41 -0200

    pesquisaPassagem
    Adicionando a pesquisa passagem

---
+++ src/components/procurarPassagem/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProcurarPassagem:React.FC = () =>{
+    return (
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div>dsda</div>
+        </div>
+    );
+}
+
+export default ProcurarPassagem;
