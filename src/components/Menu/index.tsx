a6c70ce40d169749f5542a483645fd7ee97e099f
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2020-11-14 16:13:30 -0300

    criando componente menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 
 import { MenuPerson } from './styles';
 
+import img from '../../assets/images/profiler.jpeg';
+
 const Menu: React.FC = () => {
   return (
       <>
         <MenuPerson>
-            <div>.</div>
-            <div>.</div>
+            <div className="profilePhoto">
+                <img src={img} alt=""/>
+            </div>
+            <div className="profileInfo">
+               <h2 className="name">Matheus de Oliveira</h2>
+            </div>
         </MenuPerson>
       </>
   )
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -3,15 +3,38 @@ import styled from 'styled-components';
 
 export const MenuPerson = styled.main`
         flex:1;
-        background-color:red;
+        background-color:blue;
+        width: 100%;
         height:100%;
         min-height:500px;
         display:flex;
         flex-direction:column;
+        justify-content:center;
+        align-items:flex-start;
+        padding-left:15px;
 
-        div{
+        .profilePhoto{
             flex:1;
-            background-color:yellow;
+            background-color:aqua;
+            height:210px;
+            img{
+                top:-25px;
+                position:relative;
+                width:95%;
+                height:100%;
+                object-fit: cover;
+                border-radius:50%;
+            }
         }
+        .profileInfo{
+            flex:2;
+            background-color:green;
+
+            .name{
+                color:#71838A;
+
+            }
+        }
+
 `;
 
