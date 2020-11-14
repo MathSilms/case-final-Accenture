917ef3093dc2cb32cc574939837715d6323a5c17
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2020-11-14 17:57:03 -0300

    criando menu lateral

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 
-import { MenuPerson } from './styles';
+import { LocationIcon, MailIcon, MenuPerson } from './styles';
 
 import img from '../../assets/images/profiler.jpeg';
 
 const Menu: React.FC = () => {
-  return (
-      <>
-        <MenuPerson>
-            <div className="profilePhoto">
-                <img src={img} alt=""/>
-            </div>
-            <div className="profileInfo">
-               <h2 className="name">Matheus de Oliveira</h2>
-            </div>
-        </MenuPerson>
-      </>
-  )
-  }
+
+    const [open, setOpen] = useState(false)
+
+    function handleEditUser(e:FormEvent<HTMLFormElement>){
+        e.preventDefault();
+    }
+
+    return (
+        <>
+            <MenuPerson>
+                <div className="profilePhoto">
+                    <img src={img} alt="" />
+                </div>
+                <div className="profileInfo">
+                    <h2 className="name">Matheus de Oliveira</h2>
+                    <div className="">
+                        <MailIcon size={25} />
+                    mateheusoliver@gmail.com
+                    </div>
+
+                    <div className="">
+                        <LocationIcon size={25} />
+                        Cabo Frio - Rj
+                    </div>
+                    <br/>
+                    <br/>
+                    <button onClick={e => setOpen(!open)}>Editar Perfil</button>
+                </div>
+                { open && <div className="editContent">
+
+
+                   <form  onSubmit={handleEditUser}>
+                       <input placeholder="Novo email" value="" type="text" name="" id=""/>
+                       <input placeholder="Novo nome de usuário" value="" type="text" name="" id=""/>
+                       <input placeholder="Senha antiga" value="" type="text" name="" id=""/>
+                       <input placeholder="Senha nova" value="" type="text" name="" id=""/>
+                       <div className="">
+                       <button className="save">salvar</button>
+                       <button>cancelar</button>
+                       </div>
+                   </form>
+               </div>}
+
+            </MenuPerson>
+        </>
+    )
+}
 export default Menu;
